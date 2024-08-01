a276452af8c68791a6d1d606efdf0af9c8e09f5a
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-08-01 03:51:50 +0200

    [UPD] added api call to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <script>
 import axios from 'axios';
+import store from './store.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import CardsList from './components/CardsList.vue';
 
 export default {
+
+  components: {
+    AppHeader,
+    AppMain,
+    CardsList
+  },
   
+  data() {
+    return {
+      store
+    }
+  },
+
+  created() {
+    this.getCardsList()
+  },
+
+  methods: {
+    getCardsList() {
+      axios.get(store.api).then((data) => {
+        store.cardsList = data.data.data
+
+        console.log(store.cardsList);
+        
+      });
+    }
+  },
+
 }
 </script>
 
